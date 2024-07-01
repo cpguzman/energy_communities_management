fa8bb7cdb9db0cb00d1fd1e1c7a1bfb3296225a3
Diana Estefania Cherrez Barragan <diana.cherrez@gmail.com>
2024-07-01 19:56:00 -0300

    Parameters

--- main.py
+++ main.py
@@ -47,7 +47,6 @@ dict_PstorageChargePrice = df_PstorageChargePrice.to_dict()
 dict_PstorageDischargePrice = df_PstorageDischargePrice.to_dict()
 dict_StorageCharacteritics = df_StorageCharacteritics.to_dict()
 
-
 Generators_Data_excel = pd.ExcelFile("Data/Generators_Data.xlsx")
 df_Pgen_Forecast = Generators_Data_excel.parse("1 P Forecast (kW)",index_col=0)
 df_GenCost = Generators_Data_excel.parse("3 Cost Parameter B (m.u.)",index_col=0)
@@ -63,11 +62,9 @@ df_PcsChargeLimit = ChargeStation_Data_excel.parse("1 P Charge Limit (kW)",index
 df_CS_Characteritics = ChargeStation_Data_excel.parse("3 Characteritics D.",index_col=0)
 dict_PcsChargeLimit = df_PcsChargeLimit.to_dict()
 dict_CS_Characteritics = df_CS_Characteritics.to_dict()
-
 #**************************************************************
 #                        Sets definition
 #**************************************************************
-
 model = pyo.ConcreteModel()
 model.SetTimeIntervals = pyo.Set(initialize=df_Pload_Forecast.columns, doc='Set of time intervals')
 model.LoadSet = pyo.Set(initialize=df_Pload_Forecast.index, doc='Set of Loads')
@@ -75,7 +72,56 @@ model.GenSet = pyo.Set(initialize=df_Pgen_Forecast.index, doc='Set of Generatos'
 model.BatterySet = pyo.Set(initialize=df_PstorageChargeLimit.index, doc='Set of Energy Storage System')
 model.V2G_Set = pyo.Set(initialize=df_V2G_Characteritics.index, doc='Set of Electric Vehicles')
 model.ChargeStation_Set = pyo.Set(initialize=df_CS_Characteritics.index, doc='Set of Charge Station')
+#**************************************************************
+#                        Parameters definition
+#**************************************************************
+@model.Param(model.LoadSet, model.SetTimeIntervals)
+def ParamPloadForecast_l_t(model, load, time):
+  return dict_Pload_Forecast[time][load]
 
+@model.Param(model.BatterySet, model.SetTimeIntervals)
+def ParamPstorageChargeLimit_b_t(model, storage, time):
+  return dict_PstorageChargeLimit[time][storage]
+
+@model.Param(model.BatterySet, model.SetTimeIntervals)
+def ParamPstorageDischargeLimit_b_t(model, storage, time):
+  return dict_PstorageDischargeLimit[time][storage]
+
+@model.Param(model.BatterySet)
+def EstorageMax_b(model, storage):
+  return dict_StorageCharacteritics["6 Energy Capacity (kVAh)"][storage]
+
+@model.Param(model.V2G_Set)
+def Pv2gDischargeMax_v(model, v2g):
+  return dict_V2G_Characteritics["7 P Discharge Max (kW)"][v2g]
+
+@model.Param(model.V2G_Set)
+def Pv2gChargeMax_v(model, v2g):
+  return dict_V2G_Characteritics["6 P Charge Max (kW)"][v2g]
+
+@model.Param(model.V2G_Set, model.SetTimeIntervals)
+def XconexionStatus_v_t(model, v2g, time):
+  return dict_V2G_ConexionStatus[time][v2g]
+
+@model.Param(model.V2G_Set)
+def Ev2gMax_v(model, v2g):
+  return dict_V2G_Characteritics["5 E Capacity Max (kWh)"][v2g]
+
+@model.Param(model.V2G_Set, model.SetTimeIntervals)
+def Ev2gInitial_v_t(model, v2g, time):
+  return dict_V2G_EinicialArriving[time][v2g]
+
+@model.Param(model.V2G_Set, model.SetTimeIntervals)
+def ParamEv2gExitRequired_v_t(model, v2g, time):
+  return dict_V2G_ExitRequired[time][v2g]
+
+@model.Param(model.GenSet, model.SetTimeIntervals)
+def ParamPGenForecast_g_t(model, generator, time):
+  return dict_Pgen_Forecast[time][generator]
+
+@model.Param(model.GenSet, model.SetTimeIntervals)
+def ParamPGenCost_g_t(model, generator, time):
+  return dict_Pgen_Cost[time][generator]
 
 
 
